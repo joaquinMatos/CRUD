389e40461bacfd9b4b52c4d6a4a8ea0ea3411a3e
joaquinMatos <balamema10@gmail.com>
2024-02-20 01:55:27 -0500

    first commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,37 @@
 <template>
-    <div>
-      <h1>Inicio de Sesión</h1>
-      <input v-model="loginEmail" placeholder="Correo electrónico" />
-      <input v-model="loginPassword" type="password" placeholder="Contraseña" />
-      <button @click="login">Iniciar Sesión</button>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  import { useRouter } from 'vue-router';
-  
-  const loginEmail = ref('');
-  const loginPassword = ref('');
-  const router = useRouter();
-  
-  const login = () => {
+  <div>
+    <h1>Inicio de Sesión</h1>
+    <input v-model="loginEmail" placeholder="Correo electrónico" />
+    <input v-model="loginPassword" type="password" placeholder="Contraseña" />
+    <button @click="login">Iniciar Sesión</button>
+  </div>
 
-    if (loginEmail.value === 'usuario@gmail.com' && loginPassword.value === 'secreto') {
-   
-      router.push('/usuarios');
-    } else {
-      alert('Credenciales incorrectas');
-    }
-  };
-  </script>
-  
+  <div>
+    <!-- Mostrar los datos recibidos -->
+    <p>Nombre: {{ userData.name }}</p>
+    <p>Correo electrónico: {{ userData.email }}</p>
+    <p>Contraseña: {{ userData.password }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue';
+import { useRouter } from 'vue-router';
+
+// Definir las props que esperamos recibir
+const props = defineProps({
+  userData: Object,
+});
+
+const loginEmail = ref('');
+const loginPassword = ref('');
+const router = useRouter();
+
+const login = () => {
+  if (loginEmail.value === 'usuario@gmail.com' && loginPassword.value === 'secreto') {
+    router.push('/usuarios');
+  } else {
+    alert('Credenciales incorrectas');
+  }
+};
+</script>
--- src/components/Registro.vue
+++ src/components/Registro.vue
@@ -5,26 +5,31 @@
       <input v-model="email" placeholder="Correo electrónico" />
       <input v-model="password" type="password" placeholder="Contraseña" />
       <button @click="register">Registrar</button>
+  
+      <!-- Componente hijo -->
+      <ChildComponent :userData="newUser" />
     </div>
   </template>
   
   <script setup>
+  
+  import ChildComponent from './Login.vue';
   import { ref } from 'vue';
   import { useRouter } from 'vue-router';
-  
+
   const name = ref('');
   const email = ref('');
   const password = ref('');
   const router = useRouter();
   
+  const newUser = {
+    name: name.value,
+    email: email.value,
+    password: password.value,
+  };
+  
   const register = () => {
- 
-    const newUser = {
-      name: name.value,
-      email: email.value,
-      password: password.value,
-    };
-
+    alert(`Usuario registrado:\nNombre: ${newUser.name}\nCorreo electrónico: ${newUser.email}`);
     router.push('/login');
   };
   </script>
